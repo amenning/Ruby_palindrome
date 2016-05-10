f693a7e4a901c3f77338f298335929f8a4ad97f7
metakata <menning23@gmail.com>
2016-05-10 15:48:54 -0400

    add rspec and documentation files

--- palindrome.rb
+++ palindrome.rb
@@ -1,38 +1,40 @@
-# This adds a new is_palindrome? method to the open Ruby String class.  By adding to the 
-# Ruby String class, all standard error checking methods are available for strings and
-# this method will only be available to String inputs.
+#This adds a new is_palindrome? method to the open Ruby String class.  By adding to the 
+#Ruby String class, all standard error checking methods are available for strings and
+#this method will only be available to String inputs.
 #
-# This method was implemented with the following constraints:
-# "Implement a function to determine if a string is a palindrome
-#  - ...using a constant amount of memory
-#  - ...accessing each character in the string only once"
+#This method was implemented with the following constraints:
+#"Implement a function to determine if a string is a palindrome
+#- ...using a constant amount of memory
+#- ...accessing each character in the string only once"
 #
-# The requirement to use a constant amount of memory prevents use of the simple method:
-# def is_palindrome?
-# 	return self == self.reverse
-# end
-# This is good to avoid to allow for extremely large strings as the above method would double the memory requirement 
-# by creating a new string of equal size but reversed.
+#The requirement to use a constant amount of memory prevents use of the simple method:
 #
-# The requirement to access each character only once is also beneficial as this will prevent uncessary searching
-# and iterating espcially for extremely large strings and thereby decrease computaitonal time.
+#	def is_palindrome?
+#		return self == self.reverse
+#	end
 #
-# Author::		Carl Andrew Menning 
-# Version::		0.0.1
-# License::		Distributed under the same terms as Ruby
+#This is good to avoid to allow for efficient memory use when analyzing extremely large strings as the above method would double the memory requirement 
+#by creating a new string of equal size but reversed.
+#
+#The requirement to access each character only once is also beneficial as this will prevent uncessary searching
+#and iterating espcially for extremely large strings and thereby decrease computational time.
+#
+#Author::		Carl Andrew Menning 
+#Version::		0.0.1
+#License::		Distributed under the same terms as Ruby
 class String
-	# The is_palindrome? method returns a boolean true/false depending on if the string
-	# being test is spelled the same forwards and backwards, regardless of case.  This method
-	# allows all characters to be analyzed, such as special characters, to be included in the analysis.
-	# This assumes that the test string has been stripped of all unwanted white space  or characters before being tested.
+	#The is_palindrome? method returns a boolean true/false depending on if the string
+	#being test is spelled the same forwards and backwards, regardless of case.  This method
+	#allows all characters to be analyzed, such as special characters, to be included in the analysis.
+	#This assumes that the test string has been stripped of all unwanted white space  or characters before being tested.
 	#
-	# * *Args*	  :
-	#	- String object receiver -> test string containing characters to be analyzed
-	# * *Returns* :
-	# 	- true if the string characters are symmetric with respect to the middle of the string
-	#	- false if the string is empty or is not symmetric
-	# * *Raises*  :
-	#	- This method contains no additional error checking methods
+	#*Args*	  :
+	#- String object receiver -> test string containing characters to be analyzed
+	#*Returns* :
+	#- true if the string characters are symmetric with respect to the middle of the string
+	#- false if the string is empty or is not symmetric
+	#*Raises*  :
+	#- This method contains no additional error checking
 	def is_palindrome?
 		# Assign receiver String object to test_string
 		test_string = self
---
+++ palindrome_spec.rb
@@ -0,0 +1,43 @@
+require './palindrome'
+
+describe "is_palindrome? method" do
+	it "is an added method to the open Ruby String class" do
+		test_string = String.new
+		expect(test_string).to respond_to(:is_palindrome?)
+	end
+	
+	it "is not added to other classes such as Fixnum" do
+		test_num = 13
+		expect(test_num).not_to respond_to(:is_palindrome?)
+	end	
+	
+	it "correctly identifies an even palindrome" do
+		test_string = 'abba'
+		expect(test_string.is_palindrome?).to be true
+	end	
+
+	it "correctly identifies an odd palindrome" do
+		test_string = 'abcba'
+		expect(test_string.is_palindrome?).to be true
+	end
+	
+	it "correctly identifies an even word as not a palindrome" do
+		test_string = 'abcd'
+		expect(test_string.is_palindrome?).to be false
+	end
+	
+	it "correctly identifies an odd word as not a palindrome" do
+		test_string = 'abcde'
+		expect(test_string.is_palindrome?).to be false
+	end		
+	
+	it "does not care about letter case and correctly identifies palindrome" do
+		test_string = 'AbBa'
+		expect(test_string.is_palindrome?).to be true
+	end
+	
+	it "allows special characters to be present and correctly identifies palindrome" do
+		test_string = '123AbBa321'
+		expect(test_string.is_palindrome?).to be true
+	end	
+end
